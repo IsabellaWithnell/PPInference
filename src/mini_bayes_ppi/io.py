793e0fad196e9c401eea48ac2d1a45f555cb0359
Isabella_Withnell <121874515+IsabellaWithnell@users.noreply.github.com>
2025-08-05 15:11:37 +0100

    Update io.py

--- src/mini_bayes_ppi/io.py
+++ src/mini_bayes_ppi/io.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
-from collections.abc import Iterable
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 import pandas as pd
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
 
 
 def load_string_prior(
