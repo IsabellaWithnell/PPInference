f094aefa4e2e5d8d9fcee6e0141790b6b8c8d62c
Isabella_Withnell <121874515+IsabellaWithnell@users.noreply.github.com>
2025-08-06 14:42:18 +0100

    Update __init__.py

--- src/mini_bayes_ppi/__init__.py
+++ src/mini_bayes_ppi/__init__.py
@@ -9,4 +9,27 @@ except _md.PackageNotFoundError:
 from .core import MBModel, export_networks  # noqa: F401
 from .io import load_string_prior  # noqa: F401
 
+# Import visualization functions if matplotlib is available
+try:
+    from .viz import (  # noqa: F401
+        plot_gene_coverage,
+        plot_network,
+        plot_training_history,
+        plot_edge_confidence,
+        plot_cell_type_comparison,
+    )
+    _VIZ_AVAILABLE = True
+except ImportError:
+    _VIZ_AVAILABLE = False
+
 __all__ = ["MBModel", "export_networks", "load_string_prior"]
+
+# Add viz functions to __all__ if available
+if _VIZ_AVAILABLE:
+    __all__.extend([
+        "plot_gene_coverage",
+        "plot_network", 
+        "plot_training_history",
+        "plot_edge_confidence",
+        "plot_cell_type_comparison",
+    ])
